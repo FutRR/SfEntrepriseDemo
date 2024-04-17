774d1694a485ad96f211da4e8267366ff42a9c2a
FutRR <futterermaxime@gmail.com>
2024-04-17 16:57:52 +0200

    db

--- src/Entity/Entreprise.php
+++ src/Entity/Entreprise.php
@@ -37,6 +37,7 @@ class Entreprise
      * @var Collection<int, Employe>
      */
     #[ORM\OneToMany(targetEntity: Employe::class, mappedBy: 'entreprise', orphanRemoval: true)]
+    #[ORM\OrderBy(["nom" => "ASC"])]
     private Collection $employes;
 
     public function __construct()
