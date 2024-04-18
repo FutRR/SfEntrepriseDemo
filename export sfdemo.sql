fe74f6057efaa6a95c8153ee5b3937ec134623c2
FutRR <futterermaxime@gmail.com>
2024-04-18 09:56:30 +0200

    db

--- export sfdemo.sql
+++ export sfdemo.sql
@@ -15,7 +15,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Listage de la structure de la base pour sfentreprisedemo 
+-- Listage de la structure de la base pour sfentreprisedemo
 CREATE DATABASE IF NOT EXISTS `sfentreprisedemo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `sfentreprisedemo`;
 
@@ -33,14 +33,12 @@ CREATE TABLE IF NOT EXISTS `employe` (
   CONSTRAINT `FK_F804D3B9A4AEAFEA` FOREIGN KEY (`entreprise_id`) REFERENCES `entreprise` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table sfentreprisedemo.employe : ~5 rows (environ)
+-- Listage des données de la table sfentreprisedemo.employe : ~4 rows (environ)
 INSERT IGNORE INTO `employe` (`id`, `entreprise_id`, `nom`, `prenom`, `date_naissance`, `date_embauche`, `ville`) VALUES
 	(1, 1, 'SMAIL', 'Stéphane', '1982-09-15 10:12:14', '2020-09-15 10:48:19', NULL),
 	(2, 1, 'MATHIEU', 'Quentin', '1996-09-15 10:48:45', '2021-09-15 10:48:50', NULL),
 	(3, 1, 'MURMANN', 'Mickael', '1987-09-15 10:49:13', '2010-09-15 10:49:19', NULL),
-	(4, 2, 'ANDRES', 'Mathilde', '1997-09-15 10:49:40', '2015-09-15 10:49:44', NULL),
-	(5, 5, 'Akkari', 'Zizou', '1994-09-08 00:00:00', '2023-09-14 00:00:00', NULL),
-	(6, 8, 'Bugs', 'Bunny', '1995-12-31 00:00:00', '2010-04-17 00:00:00', 'Strasbourg');
+	(4, 2, 'ANDRES', 'Mathilde', '1997-09-15 10:49:40', '2015-09-15 10:49:44', NULL);
 
 -- Listage de la structure de table sfentreprisedemo. entreprise
 CREATE TABLE IF NOT EXISTS `entreprise` (
@@ -57,17 +55,10 @@ CREATE TABLE IF NOT EXISTS `entreprise` (
 INSERT IGNORE INTO `entreprise` (`id`, `raison_sociale`, `date_creation`, `adresse`, `cp`, `ville`) VALUES
 	(1, 'ELAN FORMATION', '2024-04-15 16:43:48', '14 rue du rhöne', '67100', 'Strasbourg'),
 	(2, 'Audio Creation', '2024-04-15 16:44:19', '1 rue de l\'ecole', '68480', 'Liebsdorf'),
-	(3, 'KFC', '2011-09-15 00:00:00', '22 rue poulet frites', '67200', 'Strasbourg'),
-	(4, 'Chronopost', '2011-09-15 14:59:57', '110 route des romains', '67200', 'Strasbourg'),
-	(5, 'Laposte', '2023-09-01 00:00:00', '10 route du Rhin', '67000', 'Strasbourg'),
+	(3, 'KFC', '2011-09-15 00:00:00', '22 rue poulet braisé', '67200', 'Strasbourg'),
 	(6, 'Au coeur du chanvre', '2020-10-14 00:00:00', '246 av. de colmar', '67100', 'Strasbourg'),
-	(7, 'test', '2009-05-21 00:00:00', '1 rue du test', '68200', 'Mulhouse'),
 	(8, 'Master Kebab', '1998-01-25 00:00:00', '2 rue du crack', '75020', 'Paris'),
-	(9, 'Elan', '2004-01-01 00:00:00', '202 av de Colmar', '67200', 'STRASBOURG'),
-	(10, 'Test test', '2024-01-01 00:00:00', 'aaa', '67000', 'STRASBOURG'),
-	(11, 'bidule', '2024-01-01 00:00:00', 'aaa', '67000', 'STRASBOURG'),
-	(12, 'bidule 2', '2024-01-02 00:00:00', 'aaa', '67000', 'STRASBOURG'),
-	(13, 'bidule 4', '2024-01-01 00:00:00', 'aaa', '67000', 'STRASBOURG');
+	(9, 'Elan', '2004-01-01 00:00:00', '202 av de Colmar', '67200', 'STRASBOURG');
 
 -- Listage de la structure de table sfentreprisedemo. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
