2b94a482a25e738bd10289710f59dd143bad445d
FutRR <futterermaxime@gmail.com>
2024-04-18 15:41:54 +0200

    db exp

--- export sfdemo.sql
+++ export sfdemo.sql
@@ -19,21 +19,47 @@
 CREATE DATABASE IF NOT EXISTS `sfentreprisedemo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `sfentreprisedemo`;
 
+-- Listage de la structure de table sfentreprisedemo. categorie
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_categorie` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table sfentreprisedemo.categorie : ~0 rows (environ)
+INSERT IGNORE INTO `categorie` (`id`, `nom_categorie`) VALUES
+	(1, 'SAS'),
+	(2, 'SARL'),
+	(3, 'EI'),
+	(4, 'SA');
+
+-- Listage de la structure de table sfentreprisedemo. doctrine_migration_versions
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- Listage des données de la table sfentreprisedemo.doctrine_migration_versions : ~1 rows (environ)
+INSERT IGNORE INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+	('DoctrineMigrations\\Version20240418123326', '2024-04-18 12:33:41', 521);
+
 -- Listage de la structure de table sfentreprisedemo. employe
 CREATE TABLE IF NOT EXISTS `employe` (
   `id` int NOT NULL AUTO_INCREMENT,
   `entreprise_id` int NOT NULL,
-  `nom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prenom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_naissance` datetime DEFAULT NULL,
   `date_embauche` datetime NOT NULL,
-  `ville` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ville` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_F804D3B9A4AEAFEA` (`entreprise_id`),
   CONSTRAINT `FK_F804D3B9A4AEAFEA` FOREIGN KEY (`entreprise_id`) REFERENCES `entreprise` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table sfentreprisedemo.employe : ~4 rows (environ)
+-- Listage des données de la table sfentreprisedemo.employe : ~0 rows (environ)
 INSERT IGNORE INTO `employe` (`id`, `entreprise_id`, `nom`, `prenom`, `date_naissance`, `date_embauche`, `ville`) VALUES
 	(1, 1, 'SMAIL', 'Stéphane', '1982-09-15 10:12:14', '2020-09-15 10:48:19', NULL),
 	(2, 1, 'MATHIEU', 'Quentin', '1996-09-15 10:48:45', '2021-09-15 10:48:50', NULL),
@@ -43,29 +69,32 @@ INSERT IGNORE INTO `employe` (`id`, `entreprise_id`, `nom`, `prenom`, `date_nais
 -- Listage de la structure de table sfentreprisedemo. entreprise
 CREATE TABLE IF NOT EXISTS `entreprise` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `raison_sociale` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categorie_id` int NOT NULL DEFAULT '1',
+  `raison_sociale` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_creation` datetime NOT NULL,
-  `adresse` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cp` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ville` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `adresse` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cp` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ville` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_D19FA60BCF5E72D` (`categorie_id`),
+  CONSTRAINT `FK_D19FA60BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table sfentreprisedemo.entreprise : ~13 rows (environ)
-INSERT IGNORE INTO `entreprise` (`id`, `raison_sociale`, `date_creation`, `adresse`, `cp`, `ville`) VALUES
-	(1, 'ELAN FORMATION', '2024-04-15 16:43:48', '14 rue du rhöne', '67100', 'Strasbourg'),
-	(2, 'Audio Creation', '2024-04-15 16:44:19', '1 rue de l\'ecole', '68480', 'Liebsdorf'),
-	(3, 'KFC', '2011-09-15 00:00:00', '22 rue poulet braisé', '67200', 'Strasbourg'),
-	(6, 'Au coeur du chanvre', '2020-10-14 00:00:00', '246 av. de colmar', '67100', 'Strasbourg'),
-	(8, 'Master Kebab', '1998-01-25 00:00:00', '2 rue du crack', '75020', 'Paris'),
-	(9, 'Elan', '2004-01-01 00:00:00', '202 av de Colmar', '67200', 'STRASBOURG');
+-- Listage des données de la table sfentreprisedemo.entreprise : ~6 rows (environ)
+INSERT IGNORE INTO `entreprise` (`id`, `categorie_id`, `raison_sociale`, `date_creation`, `adresse`, `cp`, `ville`) VALUES
+	(1, 1, 'ELAN FORMATION', '2024-04-15 00:00:00', '14 rue du rhône', '67100', 'Strasbourg'),
+	(2, 3, 'Audio Creation', '2024-04-15 00:00:00', '1 rue de l\'ecole', '68480', 'Liebsdorf'),
+	(3, 1, 'KFC', '2011-09-15 00:00:00', '22 rue poulet braisé', '67200', 'Strasbourg'),
+	(6, 1, 'Au coeur du chanvre', '2020-10-14 00:00:00', '246 av. de colmar', '67100', 'Strasbourg'),
+	(8, 2, 'Master Kebab', '1998-01-25 00:00:00', '2 rue du crack', '75020', 'Paris'),
+	(9, 2, 'Elan', '2004-01-01 00:00:00', '202 av de Colmar', '67200', 'STRASBOURG');
 
 -- Listage de la structure de table sfentreprisedemo. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue_name` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
